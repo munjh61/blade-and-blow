6f9484d540a74898da717010b30674c1db0e4964
Joonho <junho9661@gmail.com>
2025-09-02 18:07:59 +0900

    세팅중 [76]
    [본문]
    application 개발환경은 -dev 아니면 prod
    [연관티켓번호]
    feat : springboot 생성, application 구현 [S13P11A405-76]

---
+++ GameDataServer/src/main/java/org/ssafy/gamedataserver/GameDataServerApplication.java
@@ -0,0 +1,13 @@
+package org.ssafy.gamedataserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDataServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameDataServerApplication.class, args);
+    }
+
+}
---
+++ GameDataServer/src/test/java/org/ssafy/gamedataserver/GameDataServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ssafy.gamedataserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameDataServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
